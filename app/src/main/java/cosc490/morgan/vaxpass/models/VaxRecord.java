b8d86bc9d5431588e417289be3321925d42037fe
Teanna Pearson <45774278+TeannaP18@users.noreply.github.com>
2021-11-16 00:05:20 -0500

    added neccessary parts for vaccination record model

--- app/src/main/java/cosc490/morgan/vaxpass/models/VaxRecord.java
+++ app/src/main/java/cosc490/morgan/vaxpass/models/VaxRecord.java
@@ -1,6 +1,23 @@
 package cosc490.morgan.vaxpass.models;
 
+import java.util.Date;
+
 public class VaxRecord {
+
+    //either pfizer, moderna, or johnson&johnson
+    public String manufacturer;
+    //date of first dose
+    public Date dose1;
+    //lot number of first dose
+    public String lotNum1;
+    //date of second dose
+    public Date dose2;
+    //lot number of second dose
+    public String lotNum2;
+    //health care professional or clinic site
+    public String site;
+    //optional notes section for patient to enter any additional details
+    public String notes;
 
 
 }
